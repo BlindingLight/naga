5aefb96fc2cefde750b819083b938a031a11de4c
Balázs <balazs.varga@penceo.com>
2014-02-21 14:21:48 +0100

    some model refactoring

--- model/Model.php
+++ model/Model.php
@@ -2,19 +2,31 @@
 
 namespace Naga\Core\Model;
 
+use Naga\Core\Database\Connection\CacheableDatabaseConnection;
 use Naga\Core\nComponent;
 
+/**
+ * Abstract class for creating models.
+ *
+ * @package Naga\Core\Model
+ * @author  BlindingLight <bloodredshade@gmail.com>
+ */
 abstract class Model extends nComponent
 {
+	/**
+	 * @var array internal data storage
+	 */
 	private $_data = array();
+	/**
+	 * @var array data key -> database field key map
+	 */
 	protected $_fieldMap = array();
-	private $_db;
 
-	public function __construct($id, $db, $load = true)
+	public function __construct($id = null, CacheableDatabaseConnection $db, $load = true)
 	{
 		$this->_data['id'] = $id;
-		$this->_db = $db;
-		if ($load)
+		$this->registerComponent('database', $db);
+		if ($id && $load)
 			$this->load();
 	}
 
@@ -23,16 +35,32 @@ abstract class Model extends nComponent
 	public abstract function delete();
 	public abstract function create();
 
+	/**
+	 * Gets the model's CacheableDatabaseConnection instance.
+	 *
+	 * @return CacheableDatabaseConnection
+	 */
 	public function db()
 	{
-		return $this->_db;
+		return $this->component('database');
 	}
 
+	/**
+	 * Gets the model's id.
+	 *
+	 * @return null|string
+	 */
 	public function id()
 	{
-		return $this->id;
+		return $this->_data['id'];
 	}
 
+	/**
+	 * Sets properties from an array.
+	 *
+	 * @param array $properties
+	 * @return $this
+	 */
 	public function mergeFrom(array $properties)
 	{
 		// filtering id
@@ -40,9 +68,16 @@ abstract class Model extends nComponent
 			unset($properties['id']);
 
 		$this->_data = array_merge($this->_data, $properties);
+
 		return $this;
 	}
 
+	/**
+	 * Gets a property.
+	 *
+	 * @param $property
+	 * @return null|mixed
+	 */
 	public function __get($property)
 	{
 		if (isset($this->_fieldMap[$property]))
@@ -51,15 +86,30 @@ abstract class Model extends nComponent
 		return isset($this->_data[$property]) ? $this->_data[$property] : null;
 	}
 
+	/**
+	 * Sets a property.
+	 *
+	 * @param $property
+	 * @param $value
+	 * @return $this
+	 * @throws \Exception
+	 */
 	public function __set($property, $value)
 	{
 		if ($property == 'id' && $this->_data['id'] != 0)
 			throw new \Exception("You can't change a model's id.");
 
 		$this->_data[$property] = $value;
+
 		return $this;
 	}
 
+	/**
+	 * Tells whether a property exists.
+	 *
+	 * @param $property
+	 * @return bool
+	 */
 	public function __isset($property)
 	{
 	  	return isset($this->_data[$property]) || isset($this->_fieldMap[$property]);
