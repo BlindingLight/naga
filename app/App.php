d52892e82ff3965f8c02b616e98821e7c8d81cec
BlindingLight <bloodredshade@gmail.com>
2014-05-17 16:39:15 +0200

    more directories

---
+++ app/App.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Naga\Core\Action\Action;
+use Naga\Core\Application;
+
+class App extends Application
+{
+	public function run()
+	{
+		// logout
+		if ($this->auth()->isLoggedIn() && $this->input()->exists('logout'))
+		{
+			$this->auth()->logout();
+			$this->cookie()->delete('rhs');
+			$this->redirect();
+		}
+
+		$defaultRoute = self::auth()->isLoggedIn()
+						? self::config()->application->get('defaultRouteIfLoggedIn')
+						: self::config()->application->get('defaultRoute');
+		self::router()->setDefaultRoute($defaultRoute);
+		$controllerResult = self::router()->routeUri();
+		if ($controllerResult instanceof Action)
+			$controllerResult->execute();
+		else if (is_string($controllerResult))
+			echo $controllerResult;
+	}
+}
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_NOTICE);
+try
+{
+	require_once('../core/bootstrap.php');
+
+	$app->run();
+	$app->finish();
+}
+catch (Exception $e)
+{
+	echo $e->getMessage();
+}
