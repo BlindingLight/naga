26ccecdd5e0c93c2648eb81300fc4504fdf3526a
BlindingLight <grc1102grC>
2015-03-29 14:10:37 +0200

    bootstrap update

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,13 +1,15 @@
 <?php
 
+/**
+ * @file Concatenates bootstrap code into app/bootstrap/_generated.php. Run this file
+ * 	every time you make changes in any of the files in app/bootstrap directory.
+ */
+
 $baseDir = __DIR__ . '/bootstrap/';
+$generatedPath = "{$baseDir}_generated.php";
 
-// checking if we need to regenerate _generated.php (php bootstrap.php --force)
-if ((!isset($argv[1]) || $argv[1] != 'update') && file_exists("{$baseDir}_generated.php"))
-	require_once "{$baseDir}_generated.php";
-
-// bootstrap files to load (ordered list)
-$loadFiles = array(
+// bootstrap files to load (please don't change the order)
+$bootstrapFiles = [
 	'autoloader',
 	'app',
 	'events',
@@ -29,16 +31,18 @@ $loadFiles = array(
 	'localization',
 	'proxyclasses',
 	'custom'
-);
+];
 
 // loading files
 $content = '';
-foreach ($loadFiles as $fileName)
+foreach ($bootstrapFiles as $fileName)
 {
 	$filePath = "{$baseDir}{$fileName}.php";
 	if (file_exists($filePath))
-		$content .= str_replace('<?php', '', file_get_contents($filePath));
+		$content .= str_replace(['<?php', '?>'], '', file_get_contents($filePath));
 }
 
 $content .= "\n\$app->profiler()->stopTimer('Bootstrap time');";
-file_put_contents("{$baseDir}_generated.php", "<?php {$content}");
+file_put_contents($generatedPath, "<?php {$content}");
+
+require_once $generatedPath;
